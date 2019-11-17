da14a3d7ff89ef6f0ea7500b4aa840c0be2948f0
ChristianOtieno <cgao273@gmail.com>
2019-11-17 14:22:23 +0300

    ADD New methods onto the existing enumerable module

--- main.rb
+++ main.rb
@@ -1,1 +1,48 @@
+module Enumerable
+  def my_each
+  
+  end
+  def my_each_with_index
+    
+  end
 
+  def my_select
+    
+  end
+
+  def my_all?
+    
+  end
+
+  def my_any?
+    
+  end
+
+  def my_none?
+    
+  end
+
+  def my_count
+    
+  end
+
+  def my_map
+    
+  end
+
+  def my_inject
+
+  end
+
+  def get_inject_and_sym
+    
+  end
+
+  def multiply_els
+    
+  end
+
+  def check_validity
+    
+  end
+end

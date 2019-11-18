ecae15ef09b7ceb4156eabd2a50d8cc8e1a82728
ChristianOtieno <cgao273@gmail.com>
2019-11-18 23:46:15 +0300

    Modify your #my_map method to take a proc instead

--- main.rb
+++ main.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 module Enumerable
   def my_each
     i = 0
-    while i < self.size
+    while i < size
       yield(self[i])
       i += 1
     end
@@ -9,7 +11,7 @@ module Enumerable
 
   def my_each_with_index
     i = 0
-    while i < self.size
+    while i < size
       yield(self[i], i)
       i += 1
     end
@@ -18,48 +20,54 @@ module Enumerable
   def my_select
     i = 0
     arr = []
-    while i < self.size
-      if yield(self[i])
-        arr << self[i]
-      end
+    while i < size
+      arr << self[i] if yield(self[i])
       i += 1
     end
     arr
   end
 
   def my_all?
-    
+    x = true
+    my_each { |e| x = false unless yield(e) }
+    x
   end
 
   def my_any?
-    
+    x = false
+    my_each { |e| x = true if yield(e) }
+    x
   end
 
   def my_none?
-    
+    x = true
+    my_each { |e| x = false if yield(e) }
+    x
   end
 
   def my_count
-    
+    total = 0
+    my_each { total += 1 }
+    total
   end
 
   def my_map
-    
+    (my_map_proc = nil)
+    x = []
+    if my_map_proc.nil?
+      my_each { |e| x << yield(e) }
+    else
+      my_each { |e| x << (my_map_proc.call e) }
+    end
+    x
   end
 
-  def my_inject
-
+  def my_inject(val)
+    my_each { |e| val = yield(val, e) }
+    val
   end
 
-  def get_inject_and_sym
-    
-  end
-
-  def multiply_els
-    
-  end
-
-  def check_validity
-    
+  def multiply_els(array)
+    array.my_inject(1) { |x, y| x * y }
   end
 end
